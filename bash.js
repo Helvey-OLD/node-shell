b673ace5bbe90e249e83c20635a4364678797691
Tim Ryding <tryding2@gmail.com>
2019-03-04 12:22:26 -0600

    second commit

--- bash.js
+++ bash.js
@@ -1,12 +1,19 @@
 //output a promt
+
+let pwd = require('./pwd.js')
+let ls = require('./ls.js')
+
 process.stdout.write('prompt > ');
-
-//The stdin 'data' evetn fires after a user types in a line
 process.stdin.on('data', (data) => {
-    //const cmd = data.toString().trim(); //remove new line.   
-
-
-
-    process.stdout.write(`Current directory: ${process.cwd()}`);
-    process.stdout.write('\nprompt > ');
+    let cmd = data.toString().trim(); //remove new line.
+    //process.stdout.write(`You typed: ${cmd}`);
+    if (cmd === 'pwd') {
+        return pwd()
+    } else if (cmd === 'ls') {
+        return ls()
+    } else {
+        process.stdout.write('\nprompt > ');
+    }
 });
+
+
